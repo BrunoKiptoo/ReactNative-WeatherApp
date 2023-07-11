646356f1e3fd94ed24a91df85dbfaa7fb225d2c3
Bruno <brunochelimo@gmail.com>
2023-07-11 11:33:21 +0300

    landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -12,42 +12,42 @@ function LandingPage() {
           <p className="text-xl mb-10">
             Unleash the power of data and revolutionize sports investment
           </p>
-          <button className="bg-purple-700 hover:bg-purple-800 text-white font-semibold py-3 px-8 rounded-lg">
+          <button className="bg-teal-600 hover:bg-teal-700 text-white font-semibold py-3 px-8 rounded-lg">
             Get Started
           </button>
         </div>
       </div>
       <div className="container mx-auto py-20">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <Link to="/analytics" className="hover:bg-purple-800">
-            <div className="bg-purple-900 text-white p-6 rounded-lg transition duration-300">
+          <Link to="/analytics" className="hover:bg-teal-700">
+            <div className="bg-teal-800 text-white p-6 rounded-lg transition duration-300 transform hover:scale-105">
               <h3 className="text-2xl font-semibold mb-4">World Records</h3>
               <p className="mb-4">
                 Explore the incredible world records set by African marathoners.
               </p>
-              <span className="text-purple-400 hover:text-purple-300">
+              <span className="text-teal-400 hover:text-teal-300">
                 Learn More
               </span>
             </div>
           </Link>
-          <Link to="/analytics" className="hover:bg-purple-800">
-            <div className="bg-purple-900 text-white p-6 rounded-lg transition duration-300">
+          <Link to="/analytics" className="hover:bg-teal-700">
+            <div className="bg-teal-800 text-white p-6 rounded-lg transition duration-300 transform hover:scale-105">
               <h3 className="text-2xl font-semibold mb-4">Running History</h3>
               <p className="mb-4">
                 Dive into the rich running history of African marathoners.
               </p>
-              <span className="text-purple-400 hover:text-purple-300">
+              <span className="text-teal-400 hover:text-teal-300">
                 Learn More
               </span>
             </div>
           </Link>
-          <Link to="/map" className="hover:bg-purple-800">
-            <div className="bg-purple-900 text-white p-6 rounded-lg transition duration-300">
+          <Link to="/map" className="hover:bg-teal-700">
+            <div className="bg-teal-800 text-white p-6 rounded-lg transition duration-300 transform hover:scale-105">
               <h3 className="text-2xl font-semibold mb-4">Athlete Profiles</h3>
               <p className="mb-4">
-                Discover talented athletes and their achievements.
+                Discover talented African athletes and their achievements.
               </p>
-              <span className="text-purple-400 hover:text-purple-300">
+              <span className="text-teal-400 hover:text-teal-300">
                 Learn More
               </span>
             </div>
